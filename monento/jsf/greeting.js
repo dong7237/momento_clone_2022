0762989d286bc42248080ab6019938ca11bb96b6
dong7237 <79515249+dong7237@users.noreply.github.com>
2022-07-26 21:42:39 +0900

    screenmax, greeting develop
    화면 맞춤및 환영인사개

--- monento/jsf/greeting.js
+++ monento/jsf/greeting.js
@@ -1,3 +1,4 @@
+const hour = new Date().getHours()
 const loginForm = document.querySelector('#login_form')
 const loginInput = document.querySelector('#input_id')
 const loginSubmit = document.querySelector('#submit_id')
@@ -8,7 +9,14 @@ function greetfunc(username) {
 
     greeting.classList.remove('hidden');
     //환영문구를 보이게 함
-    greeting.innerText = `반갑습니다 ${username}님`;
+    if (hour > 12 && hour < 18) {
+        greeting.innerText = `점심 맛있었나요? ${username}님`;
+    } else if (hour > 6 && hour <= 12) {
+        greeting.innerText = `좋은 아침입니다. ${username}님`;
+    } else {
+        greeting.innerText = `가족과 따듯한 저녁을! ${username}님`;
+    }
+
     //그리팅 클래스에 환영문구를 넣음
 }
 function loginfunc(event) {
